f52ce21cd0f97fc34ff4fbd29ddd0b23ad48d503
Anders Åberg <anders@andersaberg.com>
2016-05-09 20:43:32 +0200

    Added dist

---
+++ dist/scripts/main.js
@@ -0,0 +1,1 @@
+"use strict";$("body").colorScroll({colors:[{color:"#6114cc",position:"0vh"},{color:"#009cf3",position:"1vh"},{color:"#FFBF02",position:"2vh"},{color:"#F9615B",position:"3vh"},{color:"#000000",position:"4vh"}]}),$("h1").textillate({"in":{effect:"rollIn"}});
